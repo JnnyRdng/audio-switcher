37fe6e4c66e760be7561baa6d1c4a79e9a7db22f
Jonny Reading <jonnyreading@gmail.com>
2026-03-04 11:35:22 +0000

    Move win only constants into win file

--- src/toast/mod.rs
+++ src/toast/mod.rs
@@ -5,16 +5,6 @@ mod linux;
 
 use crate::settings::Settings;
 
-const TOAST_HEIGHT: i32 = 80;
-const TOAST_WIDTH: i32 = 350;
-const MARGIN: i32 = 20;
-const FADE_DURATION_MS: u32 = 200;
-const FADE_TICK_MS: u32 = 16;
-
-const TIMER_FADE_IN: usize = 1;
-const TIMER_HOLD: usize = 2;
-const TIMER_FADE_OUT: usize = 3;
-
 /// Show a toast notification for a device switch.
 /// Spawns a background thread, returns immediately.
 pub fn show(device_name: &str, settings: &Settings) {
